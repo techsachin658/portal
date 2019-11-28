479b08924da1127caa718ed4feffa0096a847911
Raaj <raaj@pop-os.localdomain>
2019-11-28 17:28:20 +0530

    First Commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+ <footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="#">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  Licenses
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;<?=date("Y")?> Database Portal
+          </div>
+        </div>
+      </footer>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar" >
+    <div class="logo">
+        <a href="#" class="simple-text logo-normal">
+          Portal
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item active ">
+          <a class="nav-link" href="{{route('dashboard')}}">
+              <i class="material-icons">dashboard</i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+          <a class="nav-link" href="{{route('user')}}">
+              <i class="material-icons">person</i>
+              <p>User Profile</p>
+            </a>
+          </li>
+          </ul>
+      </div>
+    </div>
